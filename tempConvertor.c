469b6912b980b0ff030fca6a604cd0e1fb367c3f
Jessie Gray <jessie.gray@ibm.com>
2022-06-14 14:23:10 -0400

    Added printf Fahrenheit function

--- tempConvertor.c
+++ tempConvertor.c
@@ -6,6 +6,7 @@ int main(void)
  
 float degrees = get_float("Celcius: ");
     int fahrenheit = (degrees * 1.8) + 32;
-    printf("Fahrenheit: %i\n", fahrenheit);
+     printf("Fahrenheit: %i\n", fahrenheit);
 
+   
 }
