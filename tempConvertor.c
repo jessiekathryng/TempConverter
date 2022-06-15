87a539b8b958314d6089adee9785697de136b07f
Jessie Gray <jessie.gray@ibm.com>
2022-06-14 22:58:40 -0400

    Updates to Github

--- tempConvertor.c
+++ tempConvertor.c
@@ -1,17 +1,23 @@
-#include <math.h>
 #include <stdio.h>
+// Converting Celsius to Farenheit
 
-
-int main(){
-
-float celsius, fahrenheit;
-
-    (celsius * 1.8 ) + 32;
-
-    printf("%f Celsius = %f Farenheit" ,celsius,fahrenheit);
-   
-   getchar();
-
-   return 0;
-
+    int main() 
+    
+{
+// assign celsius variable
+    float celsius;
+// assign fahrenheit variable
+    float fahrenheit;
+// Prompt user to enter a degree in celsius
+    printf("Celsius: ");
+// the & is used to store value given by user, to the celsius variable
+    scanf("%f", &celsius);
+// conversion equation
+   fahrenheit = (1.8) * celsius + 32;
+// return degree in farenheit
+    printf("farenheit = %f", fahrenheit);
+    
+ 
+    
+    return 0;
 }
